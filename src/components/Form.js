03df263852092060444bc868d6251ba61e96c2a4
Mohammad Arefi <m-arefi@live.com>
2022-04-29 23:28:17 +0200

    From & FormStyles finalized

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,13 +5,13 @@ const Form = () => {
   return (
     <div className='form'>
         <form>
-            <label>Your Name</label>
+            <label>Your Name:</label>
             <input type='text'></input>
-            <label>Email</label>
+            <label>Email:</label>
             <input type='email'></input>
-            <label>Inquiry</label>
+            <label>Inquiry:</label>
             <input type='text'></input>
-            <label>Message</label>
+            <label>Message:</label>
             <textarea rows='6' placeholder='Tell us more about your inquiry...' />
             <button className='btn'>Submit</button>
         </form>
