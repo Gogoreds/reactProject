14b6fd1a4d0e278089b4a018f277071ef956ef75
Mohammad Arefi <m-arefi@live.com>
2022-04-30 00:35:23 +0200

    Prettier changed something apparently

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import react from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Contact from './routes/Contact';
-import Main from './routes/Main';
-import Projects from './routes/Projects';
-import Technology from './routes/Technology';
-
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Contact from "./routes/Contact";
+import Main from "./routes/Main";
+import Projects from "./routes/Projects";
+import Technology from "./routes/Technology";
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path='/' element={<Main />} />
-        <Route path='/projects' element={<Projects />} />
-        <Route path='/technology' element={<Technology />} />
-        <Route path='/contact' element={<Contact />} />
+        <Route path="/" element={<Main />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/technology" element={<Technology />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </div>
   );
 }
-
 
 export default App;
